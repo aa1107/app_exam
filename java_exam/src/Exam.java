4b45e2128321ae68c4eed4e5137c0e79952bb7b2
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-15 14:20:06 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,12 +1,17 @@
 public class Exam{
     public static void main(String[] args){
+        /* 변수 연습 문제
+        Q1. 문자를 저장하는 변수 만ㄷ르기
+        Q2. 변수에 문자 'a'를 저장하기
+        Q3. 변수에 저장한 문자 'a'를 출력하기
+        */
         int num1;
         num1=10;
 
         int num2=20;
         int num3=num1+num2;
         System.out.println(num1+"+"+num2+"="+num3);
-        char ch1='A';
-        System.out.println(ch1);
+        char user_name='A';
+        System.out.println(user_name);
     }
 }
