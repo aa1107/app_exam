109bc5e01df0a33240a3f5bab4d918a0c79e4675
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-22 18:05:14 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,11 +1,17 @@
 public class Exam{
     public static void main(String[] args){
-        int num;
-        num = -1;
-        if(num>=0){
-            System.out.println("positive");
-        }else{
-            System.out.println("negative");
+        int num1=2;
+        while(num1<10){
+            int  num2=1;
+            System.out.println("----");
+            System.out.println(num1+"단 출력");
+            System.out.println("----");
+            while(num2<10)
+            {
+                System.out.println(num1 + "x" + num2 + "=" + (num1 * num2));
+                num2++;
+            }
+            num1++;
         }
     }
 }
