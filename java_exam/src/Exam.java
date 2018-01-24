660b10f0fba3c5cb3717dc657e171cacb3dd848b
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-24 11:53:36 +0900

    20180124-2 자바

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
-        Scanner keyboard=new Scanner(System.in);
-        int a=keyboard.nextInt();
-        for(int num=1;num<=10;num++){
-            System.out.println(a*num);
-        }
-        int num=1;
-        do{
-            System.out.println(a*num);
+        Scanner keyboard = new Scanner(System.in);
+        int num=0;
+        while(true){
+            System.out.println(num);
             num++;
-        }while (num<=10);
-        num=1;
-        while(num<=10){
-            System.out.println(a*num);
-            num++;
+            if(num==100)
+                break;
         }
     }
 }
