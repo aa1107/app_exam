59bb4d320f30fe95e72b29004aa47a0c0636353d
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-24 11:34:16 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
-        int a=4;
+        Scanner keyboard = new Scanner(System.in);
+        int a=keyboard.nextInt();
         for(int num=1;num<=10;num++){
             System.out.println(a*num);
+        }
+        int num=1;
+        do{
+            System.out.println(a*num);
+            num++;
+        }while (num<=10);
+        num=1;
+        while(num<=10){
+            System.out.println(a*num);
+            num++;
         }
     }
 }
