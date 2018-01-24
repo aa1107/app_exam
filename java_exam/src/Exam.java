d7b3587ce671fa5c91f74865f7df705b668a8755
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-24 17:58:47 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
-        int num=2;
-        while(num<10) {
-            num++;
-            if (num % 5 == 0) {
-                continue;
+        //숫자를 저장하는 변수
+        int num=1;
+        //숫자를 1씩 증가 시키면서 6의 배수이고 14의 배수인지 반복하여 무한루프로 검사
+        while(true) {
+            //숫자를 6으로 나누어서 값이 0이고 숫자를 14로 나누어서 값이 0이어야 if부분이 참
+            if (num % 6 == 0&&num%14==0) {
+                //무한루프를 탈출
+                break;
             }
-            System.out.println(num);
+            //숫자를 1 증가시킴
+            num++;
         }
+        // 6과 14의 공배수를 출력
+        System.out.println(num);
     }
 }
