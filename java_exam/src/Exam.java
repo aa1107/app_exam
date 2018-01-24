df3be2d56b1df1ce7419f371bac3475fc27b03c6
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-24 12:53:49 +0900

    20180124-3 자바

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,11 +3,11 @@ public class Exam{
     public static void main(String[] args){
         int num=2;
         while(num<10) {
+            num++;
             if (num % 5 == 0) {
-                break;
+                continue;
             }
             System.out.println(num);
-            num++;
         }
     }
 }
