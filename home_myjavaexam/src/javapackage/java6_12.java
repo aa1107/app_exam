b7629d4502987b86eea2e6cc0ac9f4aff0f06f2b
DESKTOP-LD8791U\for <ss77344@hanmail.net>
2018-01-26 08:02:04 +0900

    no message

--- home_myjavaexam/src/javapackage/java6_12.java
+++ home_myjavaexam/src/javapackage/java6_12.java
@@ -1,4 +1,7 @@
 package javapackage;
 
 public class java6_12 {
+    public static void main(String[] args){
+        System.out.println("Hello java");
+    }
 }
--- home_myjavaexam/src/myjavatest.java
+++ home_myjavaexam/src/myjavatest.java
@@ -1,2 +1,5 @@
 public class myjavatest {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
 }
