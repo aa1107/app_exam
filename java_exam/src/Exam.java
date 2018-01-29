936df402e21124a34f283b4a4d122ad260679940
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-29 17:52:37 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
-        simplefunc(0);
+        Scanner keyboard = new Scanner(System.in);
+        int inputnum1 = keyboard.nextInt();
+        int result = Fibonacci(inputnum1);
+        System.out.println(result);
     }
-    public static void simplefunc(int num){
-        System.out.println(num);
-        num++;
-        if(num==100){
-            return;
+    public static int Fibonacci(int num)
+    {
+        if(num<=0){
+            System.out.println("1이상의 정수를 입력해 주십시오.");
+            return 0;
         }
-        simplefunc(num);
+        else if(num==1&num==2)
+        {
+            num=1;
+            return num;
+        }else if(num>=3) {
+            return (Fibonacci(num - 1) + Fibonacci(num - 2));
+        }
+        return ;
     }
 }
