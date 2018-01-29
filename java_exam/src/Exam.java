0699cc6c145992a27f0d1fcde40e864d3d597f61
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-29 13:16:10 +0900

    2018-01-29-java-3

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
-        int result=adder(4,5);
-        System.out.println("4와 5의 합 : "+result);
-        System.out.println("3.5의 제곱 : "+square(3.5));
+        simplefunc(0);
     }
-
-    public static int adder(int num1, int num2)
-    {
-        int addResult=num1+num2;
-        return addResult;
-    }
-
-    public static double square(double num)
-    {
-        return num*num;
+    public static void simplefunc(int num){
+        System.out.println(num);
+        num++;
+        if(num==100){
+            return;
+        }
+        simplefunc(num);
     }
 }
