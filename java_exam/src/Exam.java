23dbb2c7857f987e97837464e2c1f4f0f20cf515
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-30 10:47:30 +0900

    Revert "2018-01-29-java-2"
    This reverts commit 4950d6ca39663e984555261ee5655c4b8f198257.

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
-        int result=adder(4,5);
-        System.out.println("4와 5의 합 : "+result);
-        System.out.println("3.5의 제곱 : "+square(3.5));
+        Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard2 = new Scanner(System.in);
+        System.out.println("프로그램의 시작");
+        System.out.println("안녕하세요 반갑습니다");
+        System.out.println("이름을 입력해 주세요");
+        String myname1 = keyboard.nextLine();
+        System.out.println("나이를 입력해 주세요");
+        int myage1 = keyboard.nextInt();
+        hiEveryone(myname1, myage1);
+        System.out.println("이름을 입력해 주세요");
+        String myname2 = keyboard2.nextLine();
+        System.out.println("나이를 입력해 주세요");
+        int myage2 = keyboard2.nextInt();
+        hiEveryone(myname2, myage2);
+        System.out.println("프로그램의 끝");
+    }
+    public static void hiEveryone(String myname, int age)
+    {
+        System.out.println("좋은 아침입니다");
+        System.out.println("제 이름은 "+ myname + "입니다");
+        System.out.println("제 나이는 " + age + "세 입니다");
     }
 
-    public static int adder(int num1, int num2)
-    {
-        int addResult=num1+num2;
-        return addResult;
-    }
-
-    public static double square(double num)
-    {
-        return num*num;
-    }
 }
