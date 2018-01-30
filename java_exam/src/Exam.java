666224bcc63217d7f56ea84f283eb1e05befc48c
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-30 17:45:32 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -2,31 +2,15 @@ import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
         Scanner kb = new Scanner(System.in);
-        System.out.println("음수라면 양수로 바꾸는 함수\n숫자를 입력하세요");
-        double input1 = kb.nextDouble();
-        System.out.println(toPositive(input1));
-        System.out.println("분모 '0'인 경우 메시지 출력해주는 함수\n분자를 입력하세요");
-        double input2 = kb.nextDouble();
-        System.out.println("분모를 입력하세요");
-        double input3 = kb.nextDouble();
-        System.out.println(divideFilter(input2,input3));
+        double input1;
+        System.out.println("원의 넓이를 구하는 함수입니다\n반지름을 입력하세요");
+        input1 = kb.nextDouble();
+        System.out.println("원의 넓이는 " + circleCal(input1) + "입니다.");
     }
 
-    public static double toPositive(double num)
-    {
-        if(num<0){
-            num= -num;
-            return num;
-        }
-        return num;
-    }
-
-    public static double divideFilter(double num1, double num2)
-    {
-        if(num2==0){
-            System.out.println("나눗셈을 할 수 없습니다.");
-            return 0;
-        }
-        return num1/num2;
+    public static double circleCal(double num1){
+        double result;
+        result = num1 * num1 * 3.14;
+        return result;
     }
 }
