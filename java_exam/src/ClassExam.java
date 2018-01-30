2e25175f63791cacc2cf714d42be455f3a0042a7
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-30 12:48:40 +0900

    2018-01-30-java-3

--- java_exam/src/ClassExam.java
+++ java_exam/src/ClassExam.java
@@ -3,11 +3,42 @@ import java.util.Scanner;
 public class ClassExam {
     public static void main(String[] args)
     {
+        String operator;
         double num1;
         double num2;
         Scanner kb = new Scanner(System.in);
+        System.out.println("첫번째 숫자를 입력하세요");
         num1 = kb.nextDouble();
+        kb.nextLine();
+        System.out.println("연산을 선택하세요(덧셈:a, 뺄셈:b, 곱셈:c, 나눗셈:d, 나머지:e)");
+        operator = kb.nextLine();
+        System.out.println("두번째 숫자를 입력하세요.");
         num2 = kb.nextDouble();
+
+        switch(operator){
+            case "a":
+                System.out.println("덧셈한 결과는 다음과 같습니다.");
+                System.out.println(adder(num1,num2));
+                break;
+            case "b":
+                System.out.println("뺄셈한 결과는 다음과 같습니다.");
+                System.out.println(minus(num1,num2));
+                break;
+            case "c":
+                System.out.println("곱셈한 결과는 다음과 같습니다.");
+                System.out.println(multi(num1,num2));
+                break;
+            case "d":
+                System.out.println("나눗셈한 결과는 다음과 같습니다.");
+                System.out.println(divide(num1,num2));
+                break;
+            case "e":
+                System.out.println("나머지한 결과는 다음과 같습니다.");
+                System.out.println(modular(num1,num2));
+                break;
+                default:
+                    System.out.println("지정된 값이 없습니다");
+        }
     }
 
     public static double adder(double num1, double num2)
--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
-        Scanner keyboard = new Scanner(System.in);
-        int inputnum1 = keyboard.nextInt();
-        int result = Fibonacci(inputnum1);
-        System.out.println(result);
+        Scanner kb = new Scanner(System.in);
+        System.out.println("음수라면 양수로 바꾸는 함수\n숫자를 입력하세요");
+        double input1 = kb.nextDouble();
+        System.out.println(toPositive(input1));
+        System.out.println("분모 '0'인 경우 메시지 출력해주는 함수\n분자를 입력하세요");
+        double input2 = kb.nextDouble();
+        System.out.println("분모를 입력하세요");
+        double input3 = kb.nextDouble();
+        System.out.println(divideFilter(input2,input3));
     }
-    public static int Fibonacci(int num)
+
+    public static double toPositive(double num)
     {
-        if(num<=0){
-            System.out.println("1이상의 정수를 입력해 주십시오.");
+        if(num<0){
+            num= -num;
+            return num;
+        }
+        return num;
+    }
+
+    public static double divideFilter(double num1, double num2)
+    {
+        if(num2==0){
+            System.out.println("나눗셈을 할 수 없습니다.");
             return 0;
         }
-        else if(num==1||num==2)
-        {
-            return 1;
-        }
-        return (Fibonacci(num - 1) + Fibonacci(num - 2));
+        return num1/num2;
     }
 }
