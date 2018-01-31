fd635c905d417de35248cb057265f475a7ebdafd
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-31 10:55:37 +0900

    2018-01-31-java-1

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
         Scanner kb = new Scanner(System.in);
-        double input1;
-        System.out.println("원의 넓이를 구하는 함수입니다\n반지름을 입력하세요");
-        input1 = kb.nextDouble();
-        System.out.println("원의 넓이는 " + circleCal(input1) + "입니다.");
+        System.out.println("1000이하의 자연수를 입력받아 1부터 차례대로 입력받은 숫자까지 더한 결과를 출력하는 프로그램\n숫자를 입력하세요");
+        int input1 = kb.nextInt();
+        System.out.println("결과 : "+addOrder(input1));
+    }
+
+    public static int addOrder(int num){
+        if(num>1000||num<0){
+            System.out.println("1000이하의 자연수를 입력하세요");
+            return 0;
+        }
+        int result=0;
+        for(int st=1;st<=num;st++){
+            result+=st;
+        }
+        return result;
     }
 
     public static double circleCal(double num1){
