3499e0a5176afca0f7486584d034897ac408a61b
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-31 13:36:26 +0900

    2018-01-31-java-3

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -2,26 +2,15 @@ import java.util.Scanner;
 public class Exam{
     public static void main(String[] args){
         Scanner kb = new Scanner(System.in);
-        System.out.println("1000이하의 자연수를 입력받아 1부터 차례대로 입력받은 숫자까지 더한 결과를 출력하는 프로그램\n숫자를 입력하세요");
-        int input1 = kb.nextInt();
-        if(input1>1000||input1<0) {
-            System.out.println("1000이하의 자연수를 입력하세요");
-        }else {
-            System.out.println("결과 : " + addOrder(input1));
-        }
+        int a = kb.nextInt();
+        repeat_10(a);
     }
 
-    public static int addOrder(int num){
-        int result=0;
-        for(int st=1;st<=num;st++){
-            result+=st;
+    public static void repeat_10(int num){
+        int num2=1;
+        while(num2<=10){
+            System.out.print(num2*num+" ");
+            num2++;
         }
-        return result;
-    }
-
-    public static double circleCal(double num1){
-        double result;
-        result = num1 * num1 * 3.14;
-        return result;
     }
 }
