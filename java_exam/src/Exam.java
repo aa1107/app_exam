cf11f0252db2e74e8d08d8460823b1c91064879d
501-03-PC\501-03 <ss77344@hanmail.net>
2018-01-31 12:53:54 +0900

    2018-01-31-java-2

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -4,14 +4,14 @@ public class Exam{
         Scanner kb = new Scanner(System.in);
         System.out.println("1000이하의 자연수를 입력받아 1부터 차례대로 입력받은 숫자까지 더한 결과를 출력하는 프로그램\n숫자를 입력하세요");
         int input1 = kb.nextInt();
-        System.out.println("결과 : "+addOrder(input1));
+        if(input1>1000||input1<0) {
+            System.out.println("1000이하의 자연수를 입력하세요");
+        }else {
+            System.out.println("결과 : " + addOrder(input1));
+        }
     }
 
     public static int addOrder(int num){
-        if(num>1000||num<0){
-            System.out.println("1000이하의 자연수를 입력하세요");
-            return 0;
-        }
         int result=0;
         for(int st=1;st<=num;st++){
             result+=st;
