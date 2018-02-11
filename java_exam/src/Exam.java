083c2b8f40c45477649349f233335ccd20baa348
DESKTOP-LD8791U\for <ss77344@hanmail.net>
2018-02-12 08:20:38 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Exam{
     public static void main(String[] args){
-        Vender myVender = new Vender();
-        Vender myVender2 = new Vender();
+        int kk=-1;
+        System.out.println("kk");
 
-        myVender.runVender();
-
-    }}
+    }
+}
