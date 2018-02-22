a83e8612b3620a2424a7545b4b62cae8d2057f28
DESKTOP-LD8791U\for <ss77344@hanmail.net>
2018-02-22 20:17:13 +0900

    no message

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Exam{
-    public static void main(String[] args){
-        int kk=-1;
-        System.out.println("kk");
+    public static void main(String[] args) throws ClassCastException{
+        int a=10;
+        if(a==10){
+            throw new ClassCastException();
+        }
+
 
     }
 }
