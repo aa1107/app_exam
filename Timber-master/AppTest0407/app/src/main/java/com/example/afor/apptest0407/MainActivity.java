f38e11f49a84124788fd9b897f1c807e367c345a
DESKTOP-LD8791U\for <ss77344@hanmail.net>
2018-04-10 22:36:49 +0900

    no message

---
+++ Timber-master/AppTest0407/app/src/main/java/com/example/afor/apptest0407/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.afor.apptest0407;
+
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
